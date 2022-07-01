e0a4e47381953a196cf51b385406d9deb09de3f9
Ingrid Echeverri <ingrid.echeverri@correounivalle.edu.co>
2022-07-01 18:55:11 -0500

    SSL

--- database/keys.js
+++ database/keys.js
@@ -14,7 +14,9 @@ const devConfig = {
 
 const proConfig = {
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL ? true : false
+  ssl: {
+    rejectUnauthorized: false,
+  }
 }
 
 const client = new Pool(process.env.NODE_ENV === "production"? proConfig: devConfig);
