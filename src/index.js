329c5ac9f49a60181d48b5666e294ee700124086
Ingrid Echeverri <ingrid.echeverri@correounivalle.edu.co>
2022-07-01 16:39:04 -0500

    heroku locally tested

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const app = require("./app")
 const port = process.env_PORT || 3000
 
 
-const server = app.listen(port,()=>{
+app.listen(port,()=>{
     console.log("Server is lisetening on port 3000")
 })
